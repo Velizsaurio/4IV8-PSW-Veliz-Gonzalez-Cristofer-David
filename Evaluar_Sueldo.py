c04c8aaba6800e0c077248f134a27d32f5a8c211
Veliz <cristof0310@gmail.com>
2022-02-16 14:19:26 -0600

    Evaluar_Sueldo.py

---
+++ Evaluar_Sueldo.py
@@ -0,0 +1,5 @@
+sueldo = int(input("Ingrese el sueldo del trabajador"))
+if (sueldo<4000):
+ print u"El aumento que recibirá es: S/",sueldo*0.15
+else:
+ print u"El aumento que recibirá es: S/",sueldo*0.08
